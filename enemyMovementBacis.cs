11b33d672d9b051728721ce4fc99c711b46d3177
DovydasJakuciunas <123966236+DovydasJakuciunas@users.noreply.github.com>
2023-10-25 18:00:53 +0100

    Enemy Follow Player
    Coded movement for the average zombie.

--- enemyMovementBacis.cs
+++ enemyMovementBacis.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class enemyMovementBacis : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public int speed;
+    public GameObject player;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 localPosition = player.transform.position - transform.position; //Takes the player position from itself
+        localPosition = localPosition.normalized;
+        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed,localPosition.z * Time.deltaTime * speed);
     }
 }
